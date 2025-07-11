af2dcf46d0edff0f0e258b7542b3f5a5f6712dc7
hailee2 <slowslow9150@gmail.com>
2025-07-11 10:57:58 +0900

    calculator class 추가

--- src/main/java/org/example/lv2/Calculator.java
+++ src/main/java/org/example/lv2/Calculator.java
@@ -3,27 +3,65 @@ package org.example.lv2;
 import java.util.Scanner;
 
 public class Calculator {
-    //1.속성
-    int num1;
-    int num2;
-    String mathSymbol;
+    Scanner scanner = new Scanner(System.in);
+
+    //속성
+    private int num1;
+    private int num2;
+    private String mathSymbol;
     int result;
 
-    //2.생성자
-
-    //scanner 메소드
-    Scanner scanner = new Scanner(System.in);
-    public int scanner1(){
-        System.out.println("첫 번째 숫자를 입력하세요 : ");
-        int num1 = scanner.nextInt();
+    //숫자 입력받기
+    public int scanner1() {
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        num1 = scanner.nextInt();
+        scanner.nextLine();  // 버퍼 비우기
         return num1;
     }
 
-    //덧셈 메소드
-    public static int add(int num1, int num2) {
-        int result = num1 + num2;
-        return result;
+    //연산자 입력
+    public String scanner2() {
+        System.out.print("사칙연산 기호(+,-,*,/)를 입력하세요 : ");
+        mathSymbol = scanner.nextLine();
+        return mathSymbol;
     }
 
-    //3.기능
+    //두번째 숫자 입력받기
+    public int scanner3() {
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        num2 = scanner.nextInt();
+        scanner.nextLine();  // 버퍼 비우기
+        return num2;
+    }
+
+    //계산수행 메소드
+    public void performCalculation(int num1, int num2, String mathSymbol) {
+
+        switch (mathSymbol) {
+            case "+":
+                this.result = num1 + num2;
+                System.out.println("결과 계산 : " + result);
+                break;
+            case "-":
+                this.result = num1 - num2;
+                System.out.println("결과 계산 : " + result);
+                break;
+            case "*":
+                this.result = num1 * num2;
+                System.out.println("결과 계산 : " + result);
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    return;
+                } else {
+                    this.result = num1 / num2;
+                    System.out.println("결과 계산 : " + result);
+                }
+                break;
+            default:
+                System.out.println("사칙연산 입력값이 올바르지 않습니다.");
+                return;
+        };
+    }
 }
