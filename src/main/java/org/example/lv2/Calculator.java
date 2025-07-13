0e324764aa740353a0c39e1b30d21943a9a91736
hailee2 <slowslow9150@gmail.com>
2025-07-13 12:54:41 +0900

    catch-try/입력값 예외처리

--- src/main/java/org/example/lv2/Calculator.java
+++ src/main/java/org/example/lv2/Calculator.java
@@ -1,6 +1,8 @@
 package org.example.lv2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Calculator {
     Scanner scanner = new Scanner(System.in);
@@ -9,29 +11,46 @@ public class Calculator {
     private int num1;
     private int num2;
     private String mathSymbol;
-    int result;
+    private int result;
 
-    //숫자 입력받기
+    //첫번째 숫자 입력받기 (숫자 외 입력시 예외 발생)
     public int scanner1() {
         System.out.print("첫 번째 숫자를 입력하세요 : ");
-        num1 = scanner.nextInt();
-        scanner.nextLine();  // 버퍼 비우기
-        return num1;
+        if (!scanner.hasNextInt()) {
+            scanner.nextLine();  // 잘못된 입력 버퍼에서 제거(int 입력값 받을 때만)
+            throw new InputMismatchException("숫자가 아닙니다. 다시 입력해주세요.");
+        }else {
+            num1 = scanner.nextInt();
+            scanner.nextLine();  // 버퍼 비우기
+            return num1;
+        }
     }
 
-    //연산자 입력
+    //연산자 입력 (연산기호 외 입력시 예외 발생)
     public String scanner2() {
         System.out.print("사칙연산 기호(+,-,*,/)를 입력하세요 : ");
         mathSymbol = scanner.nextLine();
+        Set<String> allowedOps = Set.of("+", "-", "*", "/"); //연산자 기호 컬렉션에 모아놓기
+        if (!allowedOps.contains(mathSymbol)) {  //연산자가 아닌 입력값을 입력했을 시
+            throw new IllegalArgumentException("허용되지 않은 연산자입니다."); //예외 만들기
+        }
         return mathSymbol;
     }
 
     //두번째 숫자 입력받기
     public int scanner3() {
         System.out.print("두 번째 숫자를 입력하세요 : ");
-        num2 = scanner.nextInt();
-        scanner.nextLine();  // 버퍼 비우기
-        return num2;
+        if (!scanner.hasNextInt()) {
+            scanner.nextLine();  // 잘못된 입력 버퍼에서 제거(int 입력값 받을 때만)
+            throw new InputMismatchException("숫자가 아닙니다. 다시 입력해주세요.");
+        }else {
+            num2 = scanner.nextInt();
+            scanner.nextLine();  // 버퍼 비우기
+            if (mathSymbol.equals("/")&& num2 == 0) {
+                throw new IllegalArgumentException("나눗셈 두번째 숫자 0은 입력할 수 없습니다. 다시 입력해주세요.");
+            }
+            return num2;
+        }
     }
 
     //계산수행 메소드
@@ -52,8 +71,7 @@ public class Calculator {
                 break;
             case "/":
                 if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    return;
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
                 } else {
                     this.result = num1 / num2;
                     System.out.println("결과 계산 : " + result);
--- src/main/java/org/example/lv2/main.java
+++ src/main/java/org/example/lv2/main.java
@@ -1,5 +1,6 @@
 package org.example.lv2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
@@ -9,10 +10,36 @@ public class main {
         String exit = "";
 
         do {
-            //슷자, 연산기호 입력받기
-            int num1 = calculator.scanner1();
-            String mathSymbol = calculator.scanner2();
-            int num2 = calculator.scanner3();
+            //첫번째 숫자 입력받기, 숫자 외 입력시 예외처리
+            int num1 = 0; //여기서 선언안해주면 밑에 연산수행 .... 모르겠네 이 개념 다시 복습하기 ****************
+                while(true) { //예외 발생 시 다시 처음으로 돌아가도록 루프 설정
+                    try {
+                        num1 = calculator.scanner1();
+                        break; //올바른 값 입력했을 경우 무한 루프 벗어나기
+                    } catch (InputMismatchException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            //연산기호 입력받기, 기호 외 입력시 예외처리
+            String mathSymbol="";
+            while(true) {
+                try {
+                    mathSymbol = calculator.scanner2();
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            int num2 = 0;
+            while(true){
+                try {
+                    num2 = calculator.scanner3();
+                    break;
+                } catch (InputMismatchException | IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
 
             // 연산 수행
             calculator.performCalculation(num1, num2, mathSymbol);
